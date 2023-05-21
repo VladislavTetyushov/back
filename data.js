ed76df7b7c3d726c63b5bf03dd2decf388fffc35
VladislavTetyushov <tetyushov@gmail.com>
2023-05-21 21:58:38 +0500

    Сделал wordSets, word, и запросы по кол-ву нужных карточек

--- data.js
+++
@@ -1,7 +0,0 @@
-const data = [
-    { word: "house", translation: "дом" },
-    { word: "car", translation: "машина" },
-    { word: "red", translation: "красный" }
-];
-
-module.exports = data;
---
+++ words.js
@@ -0,0 +1,137 @@
+let img = '/assets/images';
+
+const words = [
+    {
+        word: 'house',
+        transcript: '[haus]',
+        translate: 'дом',
+        img: img + '/house.jpg',
+        themes: [
+            'house',
+            'art'
+        ]
+
+    },
+    {
+        word: 'apartment',
+        transcript: '[ə’pɑːtmənt]',
+        translate: 'квартира',
+        img: img + '/apartment.jpg',
+        themes: [
+            'house',
+            'art'
+        ]
+    },
+    {
+        word: 'room',
+        transcript: '[ruːm]',
+        translate: 'дом',
+        img: img + '/room.jpg',
+        themes: [
+            'house',
+            'elementary'
+        ]
+    },
+    {
+        word: 'bathroom',
+        transcript: '[‘bɑːθruːm]',
+        translate: 'ванная комната',
+        img: img + '/bathroom.jpg',
+        themes: [
+            'house',
+            'elementary'
+        ]
+    },
+    {
+        word: 'balcony',
+        transcript: '[‘bælkənɪ]',
+        translate: 'балкон',
+        img: img + '/balcony.jpg',
+        themes: [
+            'house',
+            'elementary'
+        ]
+    },
+    {
+        word: 'bedroom',
+        transcript: '[‘bedruːm]',
+        translate: 'спальня',
+        img: img + '/bedroom.jpg',
+        themes: [
+            'house',
+            'elementary'
+        ]
+    },
+    {
+        word: 'closet',
+        transcript: '[‘klɔzɪt]',
+        translate: 'шкаф',
+        img: img + '/closet.jpg',
+        themes: [
+            'house',
+            'elementary'
+        ]
+    },
+    {
+        word: 'dining room',
+        transcript: '[‘daɪnɪŋˌrum]',
+        translate: 'столовая',
+        img: img + '/dining_room.jpg',
+        themes: [
+            'house',
+            'elementary'
+        ]
+    },
+    {
+        word: 'doorbell',
+        transcript: '[‘dɔːbel]',
+        translate: 'дверной звонок',
+        img: img + '/doorbell.jpg',
+        themes: [
+            'house',
+            'elementary'
+        ]
+    },
+    {
+        word: 'upstairs',
+        transcript: '[ʌp’steəz]',
+        translate: 'верхний этаж',
+        img: img + '/upstairs.jpg',
+        themes: [
+            'house',
+            'elementary'
+        ]
+    },
+    {
+        word: 'downstairs',
+        transcript: '[‘daun’steəz]',
+        translate: 'подвальный этаж',
+        img: img + '/downstairs.jpg',
+        themes: [
+            'house',
+            'elementary'
+        ]
+    },
+    {
+        word: 'roof',
+        transcript: '[ruːf]',
+        translate: 'крыша, кровля',
+        img: img + '/roof.jpg',
+        themes: [
+            'house',
+            'elementary'
+        ]
+    },
+    {
+        word: 'window',
+        transcript: '[‘wɪndəu]',
+        translate: 'окно',
+        img: img + '/window.jpg',
+        themes: [
+            'house',
+            'elementary'
+        ]
+    }
+]
+
+module.exports = words;

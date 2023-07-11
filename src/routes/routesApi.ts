c8b1d0db6ef2d80c4b552191f0855c6ece2f0d3d
Александр Макаров <alexmk.work@gmail.com>
2023-07-11 14:13:04 +0500

    фикс роутинга логина и регистрации

--- src/routes/routesApi.ts
+++ src/routes/routesApi.ts
@@ -5,6 +5,7 @@ import * as controllerReviews from "@controllers/controllerReviews.js";
 import * as controllerWords from "@controllers/controllerWords.js";
 import * as controllerWordSets from "@controllers/controllerWordSets.js";
 import * as controllerFeedback from "@controllers/controllerFeedback.js";
+import * as controllerUsers from "@controllers/controllerUsers.js";
 
 router.use(express.json());
 
@@ -19,5 +20,8 @@ router.get("/api/train-en-ru", controllerWordSets.trainEnRuGet);
 router.get("/api/train-ru-en", controllerWordSets.trainRuEnGet);
 
 router.post("/api/feedback", controllerFeedback.feedbackPost);
+
+router.post("/api/login", controllerUsers.login);
+router.post("/api/register", controllerUsers.register);
 
 export default router;
